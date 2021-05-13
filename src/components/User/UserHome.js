5f341ff2d3c08c0e994c47e3e4340f2c2d584d53
LeahESc <leahemily1@gmail.com>
2021-05-13 09:55:58 -0700

    centers user home items

--- src/components/User/UserHome.js
+++ src/components/User/UserHome.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Button from '../Button/Button.js'
 
 const UserHome = (props) => {
-    if(props.user) { 
+    if(props.user.username) { 
     return (
-        <div>
+        <div className="user--home"> 
             <h1 className="title"> Hello {props.user.username}</h1>
         
         <Button>Past Pulls</Button>
@@ -14,8 +14,8 @@ const UserHome = (props) => {
     )
     } else { 
         return (
-            <div>
-                <h1> Please login or signup</h1>
+            <div className="user--home">
+                <h1 className="title"> Please login or signup</h1>
             </div>
         )
     }
