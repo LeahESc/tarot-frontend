78176f70aee75b949acc51fb0ad53b3dcb6a9559
LeahESc <leahemily1@gmail.com>
2021-05-13 10:02:30 -0700

    adds navbar styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,29 @@ import React from 'react'
 import { Navbar, Button, Nav } from 'react-bootstrap'
 
 const Header = () =>  { 
+    
+    const navStyle = { 
+        fontSize: "1.5rem",
+        color: "black",
+        fontWeight: "bold"
+    }
+
+    const logoStyle = {
+        fontSize: "2rem"
+    }
+
+    const navLinkStyle = {
+        marginLeft: ".75rem"
+    }
 
     return(
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">Small Spells Tarot</Navbar.Brand>
+        <Navbar style={navStyle} bg="light" expand="lg">
+            <Navbar.Brand style={logoStyle} href="/">Small Spells Tarot</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
-                    <Nav.Link href="/Login">Login</Nav.Link>
+                    <Nav.Link style={navLinkStyle} href="/home">Home</Nav.Link>
+                    <Nav.Link style={navLinkStyle} href="/Login">Login</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
