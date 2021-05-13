a83c5984dea0b4cb175e8da1a674530848a436cb
LeahESc <leahemily1@gmail.com>
2021-05-13 14:00:25 -0700

    reformats signup page

--- src/components/Registration/Signup.js
+++ src/components/Registration/Signup.js
@@ -70,61 +70,61 @@ const Signup = (props) => {
     }
 
     return (
-        <div className="loginContainer">
+    <div className="loginContainer">
         <div className="content">
-        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
-            <Form onSubmit={handleSubmit}>
-                <h2>Sign Up</h2>
-            <Form.Group>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control 
-                        type="username"
-                        id="username"
-                        name="username" 
-                        placeholder="Create Username" 
-                        onChange = {handleChange}
-                         />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                        type="email"
-                        id="email"
-                        name="email" 
-                        placeholder="Enter email" 
-                        onChange = {handleChange}/>
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+            <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+                <Form onSubmit={handleSubmit}>
+                    <h2>Sign Up</h2>
+                    <Form.Group>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control 
+                            type="username"
+                            id="username"
+                            name="username" 
+                            placeholder="Create Username" 
+                            onChange = {handleChange}
+                            />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control 
+                            type="email"
+                            id="email"
+                            name="email" 
+                            placeholder="Enter email" 
+                            onChange = {handleChange}/>
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                    </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                        type="password" 
-                        id="password"
-                        name="password"
-                        placeholder="Password" 
-                        onChange = {handleChange}/>
-                </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control 
+                            type="password" 
+                            id="password"
+                            name="password"
+                            placeholder="Password" 
+                            onChange = {handleChange}/>
+                    </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control 
-                        type="password" 
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        placeholder="Confirm Password" 
-                        onChange = {handleChange}/>
-                </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control 
+                            type="password" 
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            placeholder="Confirm Password" 
+                            onChange = {handleChange}/>
+                    </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
                 </Form>
+            </div>
         </div>
-        </div>
-        </div>
+    </div>
 
     )
 
